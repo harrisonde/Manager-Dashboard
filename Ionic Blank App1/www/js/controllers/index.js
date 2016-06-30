223fac0b0dd1385636e10e9d4fe80d0f1000808c
brownre <brownre@BROWNRE2>
2016-06-30 17:49:55 -0400

    Add project files.

---
+++ Ionic Blank App1/www/js/controllers/index.js
@@ -0,0 +1,7 @@
+﻿angular
+    .module('starter',[])
+	.controller('IndexController', ['$scope', function ($scope, Login) {
+    	$scope.submitLogin = function () {
+    	    Login.getAppKey();
+    	}
+    }]);
---
+++ Ionic Blank App1/www/js/services/login.js
@@ -0,0 +1,18 @@
+﻿angular
+    .module('starter',[])
+    .service('Login', function ($http) {
+        this.getAppKey = function () {
+            /*var data = $.param({
+                EMAIL_ID: "reginald.brown@hpe.com",
+                SCOPE: "ZohoCRM/crmapi",
+                PASSWORD: "b5X^H9vWO^k9PIx",
+                DISPLAY_NAME: "Manager Dashboard App"
+            });
+
+            $http.post('https://accounts.zoho.com/apiauthtoken/nb/create', data).then(function (respponse) {
+                return response;
+            });*/
+
+            console.log("Hello");
+        }
+});
